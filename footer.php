f48a46a3478a1fbe10564c75a02fdf7078665295
rodrigopulse <contato@rodrigogodoy.net>
2019-03-14 09:27:58 -0300

    Colocado o form de contato e ajustes de margem

--- footer.php
+++ footer.php
@@ -1,3 +1,18 @@
+<div class="container-fluid bg-cor-escura">
+    <div class="row row--sessoes align-items-center" style="max-width: 800px;">
+        <div class="col-sm-12">
+            <div class="titulo-padrao-sessao titulo-padrao-sessao--mais-escuro">
+                <h3>Contato</h3>
+                <h4>Vamos trabalhar juntos?</h4>
+            </div>
+        </div>
+    </div>
+    <div class="row row--sessoes"  style="max-width: 800px; margin: 0 auto;">
+        <?php echo do_shortcode('[contact-form-7 id="19" title="Formulário de contato"]'); ?>
+    </div>
+</div>
+
 <?php wp_footer(); ?>
+
 </body>
 </html>
--- template_partes/pagina-inicial.php
+++ template_partes/pagina-inicial.php
@@ -7,20 +7,22 @@
     }
 ?>
 <div class="destaque parallax" style="background-image: url(<?php echo $thumbDestaque; ?>);">
-    <div class="destaque__conteudo">
-        <h1 class="destaque__titulo">
-            Design e Tecnologia
-        </h1>
-        <h2 class="destaque__subtitulo">
-            UX / UI / DESENVOLVIMENTO / 3d / VÍDEO / MOTION
-        </h2>
-        <h3 class="destaque__descricao">
-            Tire seus projetos do papel e dê vida para eles.
-        </h3>
-        <a class="botao-padrao botao-padrao--branco" href="">
-            <div class="botao-padrao__risca"></div>
-            Saiba Mais
-        </a>
+    <div class="row row--max-width">
+        <div class="destaque__conteudo">
+            <h1 class="destaque__titulo">
+                Design e Tecnologia
+            </h1>
+            <h2 class="destaque__subtitulo">
+                UX / UI / DESENVOLVIMENTO / 3d / VÍDEO / MOTION
+            </h2>
+            <h3 class="destaque__descricao">
+                Tire seus projetos do papel e dê vida para eles.
+            </h3>
+            <a class="botao-padrao botao-padrao--branco" href="">
+                <div class="botao-padrao__risca"></div>
+                Saiba Mais
+            </a>
+        </div>
     </div>
 </div>
 <div class="container-fluid bg-cor-primaria">
@@ -95,7 +97,7 @@
         </a>
     </div>
 </div>
-<div class="container-fluid bg-cor-primaria">
+<div class="container-fluid bg-cor-primaria" style="padding-bottom: 50px;">
     <div class="row row--sessoes align-items-center">
         <div class="col-sm-12">
             <div class="titulo-padrao-sessao titulo-padrao-sessao--escuro">
