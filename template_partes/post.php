66ee463339aadf24b121ba69141bbaf7fda7536b
rodrigopulse <contato@rodrigogodoy.net>
2019-03-15 12:13:41 -0300

    Título dos posts na lateral

--- template_partes/post.php
+++ template_partes/post.php
@@ -2,8 +2,11 @@
     <?php
     if ( have_posts() ) {
         while ( have_posts() ) { the_post();?>
-            <h1 class="titulo-do-post"><?php echo get_the_title(); ?></h1>
-            <article>
+            <div class="container-titulo-post">
+                <h1 class="titulo-do-post"><?php echo get_the_title(); ?></h1>
+                <h2 class="subtitulo-do-post"><?php echo get_the_excerpt(); ?></h2>
+            </div>
+            <article class="post-content">
                 <?php the_content(); ?>
             </article>
         <?php }
