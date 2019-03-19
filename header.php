9b1f193f85c54953379fa2fabd97b85a757e8ac2
rodrigopulse <contato@rodrigogodoy.net>
2019-03-19 09:18:11 -0300

    colocado favicon

--- header.php
+++ header.php
@@ -3,7 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="theme-color" content="#222222">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo( 'template_url' ); ?>/assets/imagens/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo( 'template_url' ); ?>/assets/imagens/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo( 'template_url' ); ?>/assets/imagens/favicon-16x16.png">
+	<link rel="manifest" href="<?php bloginfo( 'template_url' ); ?>/site.webmanifest">
+	<link rel="mask-icon" href="<?php bloginfo( 'template_url' ); ?>/assets/imagens/safari-pinned-tab.svg" color="#333333">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="theme-color" content="#333333">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
